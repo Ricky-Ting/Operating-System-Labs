0b626037f82d5506039a2eb875f9cb2891de0a26
tracer-nju <tracer@nju.edu.cn>
2019-02-26 20:19:24 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 89f50163af124356af69179a64eb8eba     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  20:19:24 up  2:29,  5 users,  load average: 0.16, 0.16, 0.14

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -9,9 +9,12 @@ struct node {
 	pid_t pid_num;
 	pid_t pid_p;
 	char pid_name[100] ;
+	int child_num;
+	int child_index[200];
 } pidtree[MAXPID];
 
 int counter = 0;
+int root;
 void pstree(void);
 
 int main(int argc, char *argv[]) {
@@ -71,10 +74,32 @@ void pstree() {
 	}
 	closedir(dir);
 
-	
+	/*	
 	for(int i=0;i<counter;i++) {
 		printf("pid:%d,name:%s,parent:%d\n",pidtree[i].pid_num,pidtree[i].pid_name, pidtree[i].pid_p);
 	}
 	
 }
+	*/
+	for(int i=0;i<counter;i++) {
+		pidtree[i].child_num=0;
+	}	
+
+	for(int i=0;i<counter;i++) {	
+		if(pidtree[i].pid_p==0) {
+			root = i;
+		}
+		else {
+			for(int j=0;j<counter;j++){
+				if(pidtree[j].pid_num==pidtree[i].pid_p) {
+					pidtree[j].child_index[pidtree[j].child_num]=i;
+					pidtree[j].child_num++;
+				}
+			}
+
+		}
+	}
+
+
+
 
