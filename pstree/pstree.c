5758b7dc0cb99b4203a3f159f469f3e1ebc40c04
tracer-nju <tracer@nju.edu.cn>
2019-02-26 19:02:50 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 89f50163af124356af69179a64eb8eba     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  19:02:50 up  1:12,  4 users,  load average: 0.02, 0.04, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <assert.h>
 
+void pstree(void);
+
 int main(int argc, char *argv[]) {
-  printf("Hello, World!\n");
   int i;
   for (i = 0; i < argc; i++) {
     assert(argv[i]); // always true
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]); // always true
+	pstree();
+
   return 0;
 }
+
+void pstree() {
+	DIR *dir = opendir("/proc");
+	struct dirent * ptr;
+	while( ptr=readdir(dir) ) {
+		if(ptr->d_type==DT_DIR)
+			printf("%s\n",ptr->d_name);
+
+	}
+
+}
+
