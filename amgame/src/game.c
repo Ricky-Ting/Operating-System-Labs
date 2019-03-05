46878c2a09efa3a3a7c289f1c3bd0395f1aba7c6
tracer-nju <tracer@nju.edu.cn>
2019-03-05 21:05:56 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 09e6884aba1140e78777cb1a1eae58b3     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  21:05:56 up  2:36,  6 users,  load average: 0.01, 0.05, 0.07

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -2,20 +2,48 @@
 
 void init_screen();
 void splash();
-void read_key();
-
+int read_key();
+void move(int key_dire);
+int in_bound(int bx, int by);
+enum {	_UP=0, _DOWN=1, _LEFT=2, _RIGHT=3 };
+int dire[4][2]= { {0,-1}, {0,1}, {-1,0}, {1,0} };
+int w,h;
+int px,py;
 int main() {
   // Operating system is a C program
   _ioe_init();
   init_screen();
+	px = w/2; py = h/2;
   splash();
   while (1) {
-    read_key();
+    int key = read_key();
+		if(key != _KEY_NONE) {
+				switch(key) {
+					case _KEY_UP:   	move(_UP); 		break;	
+					case _KEY_DOWN:	move(_DOWN);	break;
+					case _KEY_LEFT:	move(_LEFT);	break;
+					case _KEY_RIGHT:	move(_RIGHT);	break;
+					default: break;
+				}
+		}
   }
   return 0;
 }
 
-void read_key() {
+void move(int key_dire) {
+	if( in_bound(px+dire[key_dire][0],py+dire[key_dire][1]) ) {
+		px+=dire[key_dire][0];
+		py+=dire[key_dire][1];
+	}	
+}
+
+int in_bound(int bx, int by) {
+	if( bx>=0 && bx*SIDE<=w  && by>=0 && by*SIDE<=h )
+		return 1;
+	return 0;
+}
+
+int read_key() {
   _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
   #define KEYNAME(key) \
     [_KEY_##key] = #key,
@@ -27,10 +55,11 @@ void read_key() {
     puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\n");
+		return event.keycode;
   }
+	return _KEY_NONE;
 }
 
-int w, h;
 
 void init_screen() {
   _DEV_VIDEO_INFO_t info = {0};
@@ -51,7 +80,9 @@ void draw_rect(int x, int y, int w, int h, uint32_t color) {
   _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &event, sizeof(event));
 }
 
+
 void splash() {
+/*
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       if ((x & 1) ^ (y & 1)) {
@@ -59,4 +90,8 @@ void splash() {
       }
     }
   }
+*/
+	draw_rect(px*SIDE, py*SIDE, SIDE, SIDE, 0xffffff); 
 }
+
+
