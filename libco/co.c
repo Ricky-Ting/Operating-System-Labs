91922dca258bec1b7a4f6c53a37414cb832c9367
tracer-nju <tracer@nju.edu.cn>
2019-03-12 21:50:45 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: f89cf01059b54c1da30ebcc6efce5e84     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  21:50:45 up  3:16,  3 users,  load average: 0.11, 0.15, 0.12

--- libco/co.c
+++ libco/co.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
+#include <assert.h>
 #include "co.h"
+#include <ucontext.h>
+#include <time.h>
+#include <stdlib.h>
+#define KB * 1024LL
+#define MB KB * 1024LL
+#define GB MB * 1024LL
+
+#define CO_MAX 100;
 
 struct co {
+	ucontext_t uc;
+	int co_index;
+	char __stack[10 MB];
+	
 };
 
+co * co_array[CO_MAX];
+int co_counter;
 void co_init() {
+	srand(time(NULL));
+	co_counter=0;
+	for(int i=0; i<CO_MAX; i++) 
+		co_array[i]=NULL;
 }
 
 struct co* co_start(const char *name, func_t func, void *arg) {
-  func(arg); // Test #2 hangs
-  return NULL;
+  //func(arg); // Test #2 hangs
+	co* new_co = malloc(sizeof(co));
+	getcontext(&(co->uc));	
+	makecontext(&(co->uc), func, 1, (void *)arg);
+	co->co_index = co_counter;
+	assert(co_counter<100);
+	co_array[co_counter++] = co;
+  return co;
 }
 
 void co_yield() {
+	int next_co;
+	do {
+		next_co = rand()%co_counter;
+	} while(co_array[next_co]==NULL)
+	setcontext(co_array[next_co]->uc);
+	return ;	
 }
 
 void co_wait(struct co *thd) {
+	ucontext_t * thisuc = malloc(sizeof(ucontext_t));  
+	thd->uc->uc_link = thisuc;
+	swapcontext(thisuc, &(thd->uc));
+	
+	free(thisuc);
+	co_array[thd->co_index]=NULL;
+	free(thd);
+	return;	
 }
 
