1faf96697616cfea822b201d69eba69000e64859
tracer-nju <tracer@nju.edu.cn>
2019-03-13 11:09:18 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: e1b24553caff46b99e2cceb071453269     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  11:09:18 up 28 min,  1 user,  load average: 0.03, 0.07, 0.14

--- libco/co.c
+++ libco/co.c
@@ -9,8 +9,7 @@
 #define GB MB * 1024LL
 
 #define CO_MAX 100
-ucontext_t  thisuc; //= malloc(sizeof(ucontext_t));  
-	ucontext_t  beforeuc; //= malloc(sizeof(ucontext_t));  
+ucontext_t  main_uc; 
 
 struct co {
 	ucontext_t uc;
@@ -19,16 +18,17 @@ struct co {
 	char __stack[100 MB];	
 };
 
-struct co * co_array[CO_MAX+1];
+struct co * co_array[CO_MAX];
 int co_counter;
-int current;
+ucontext_t * current;
 void co_init() {
 	srand(time(NULL));
 	co_counter=0;
-	current =-1;
+	current = &main_uc;
 	for(int i=0; i<CO_MAX; i++) 
 		co_array[i]=NULL;
 
+	
 	printf("Has inited\n");
 }
 
@@ -38,60 +38,43 @@ struct co* co_start(const char *name, func_t func, void *arg) {
 	getcontext(&(new_co->uc));	
 	new_co->uc.uc_stack.ss_sp = new_co->__stack;
 	new_co->uc.uc_stack.ss_size = sizeof(new_co->__stack);
-	new_co->uc.uc_link = &thisuc;
+	new_co->uc.uc_link = &main_uc;
 	makecontext(&(new_co->uc), (void(*) (void ) )func, 1, (void *)arg);
 	new_co->co_index = co_counter;
 	sprintf(new_co->thread_name,"%s",name);
-	assert(co_counter<100);
+	assert(co_counter<CO_MAX);
 	co_array[co_counter++] = new_co;
 	printf("co_start: create %s, counter=%d\n",name,co_counter);
+	swapcontext(&main_uc, &(new_co->uc));
   return new_co;
 }
 
 void co_yield() {
-	int ccurrent = current;
+	ucontext_t * ccurrent = current;
 	int next_co;
 	do {
 		next_co = rand()%co_counter;
+		
 	} while(co_array[next_co]==NULL);
-	current = next_co;
-	//getcontext(&(co_array[ccurrent]->uc));
-	//setcontext(&(co_array[next_co]->uc));	
-	swapcontext(&(co_array[ccurrent]->uc)  , &(co_array[next_co]->uc));
+		
+	
+	current = &(co_array[next_co]->uc);
+	swapcontext(ccurrent  , current);
 	return ;	
 }
 
 void co_wait(struct co *thd) {
-	int ccurrent = current;	
+	ucontext_t * ccurrent = current;	
 	//printf("In co_wait: wait for %s\n",thd->thread_name);
-	//ucontext_t * thisuc = malloc(sizeof(ucontext_t));  
-	//ucontext_t * beforeuc = malloc(sizeof(ucontext_t));  
-	(thd->uc).uc_link = &thisuc;
-	for(int i=0; i<co_counter; i++) {
-		if(co_array[i]==thd)
-			current = i;
-	}
-	for(int i=0; i<co_counter; i++) {
-		if(co_array[i]!=NULL && i!=current) {
-			(co_array[i]->uc).uc_link = &beforeuc;
-		}	
-	}
-	getcontext(&beforeuc);
-	getcontext(&thisuc);
+	current = &(thd->uc);
+	//getcontext(&thisuc);
 	printf("In co_wait: wait for %s\n",thd->thread_name);
 
 
-	int ret=setcontext(/*&thisuc,*/ &(thd->uc));
+	int ret=swapcontext(ccurrent, current);
 	assert(ret!=-1);
 	printf("In co_wait: %s returned\n", thd->thread_name);	
-	for(int i=0; i<co_counter; i++) {
-		if(co_array[i]!=NULL && i!=current) {
-			(co_array[i]->uc).uc_link = NULL;
-		}	
-	}
 	current = ccurrent;	
-	//free(thisuc);
-	//free(beforeuc);
 	co_array[thd->co_index]=NULL;
 	free(thd);
 	return;	
