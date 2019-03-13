83d2e4e50cff05374a9323fa731b34ecc815f75c
tracer-nju <tracer@nju.edu.cn>
2019-03-13 11:34:09 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: e1b24553caff46b99e2cceb071453269     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  11:34:09 up 53 min,  1 user,  load average: 0.13, 0.13, 0.15

--- libco/co.c
+++ libco/co.c
@@ -10,10 +10,11 @@
 
 #define CO_MAX 100
 ucontext_t  main_uc; 
-
+ucontext_t * main_wait;
 struct co {
 	ucontext_t uc;
 	int co_index;
+	int wait;
 	char thread_name[20];
 	char __stack[100 MB];	
 };
@@ -21,10 +22,34 @@ struct co {
 struct co * co_array[CO_MAX];
 int co_counter;
 ucontext_t * current;
+void myfunc(func_t func, void *arg){
+	func(arg);
+	if(main_wait == current) {
+		main_wait = NULL;
+	}	
+	else {
+		for(int i=0;i<co_counter;i++) {
+			if( (co_array[i]!=NULL) && (co_array[i]->wait == current)   )
+				co_array[i]->wait=NULL;
+		}
+	}
+	
+	for(int i=0;i<co_counter;i++) {
+		if( (co_array[i]!=NULL) && (&(co_array[i]->uc)==current)  )
+			current_index = i;
+	}	
+
+	free(co_array[current_index]);
+	co_array[current_index]=NULL;
+
+	co_yield();	
+
+}
 void co_init() {
 	srand(time(NULL));
 	co_counter=0;
 	current = &main_uc;
+	main_wait = NULL;
 	for(int i=0; i<CO_MAX; i++) 
 		co_array[i]=NULL;
 
@@ -36,10 +61,11 @@ struct co* co_start(const char *name, func_t func, void *arg) {
   //func(arg); // Test #2 hangs
 	struct co* new_co = malloc(sizeof(struct co));
 	getcontext(&(new_co->uc));	
+	new_co->wait=NULL;
 	new_co->uc.uc_stack.ss_sp = new_co->__stack;
 	new_co->uc.uc_stack.ss_size = sizeof(new_co->__stack);
 	new_co->uc.uc_link = &main_uc;
-	makecontext(&(new_co->uc), (void(*) (void ) )func, 1, (void *)arg);
+	makecontext(&(new_co->uc), myfunc, 2, func ,(void *)arg);
 	new_co->co_index = co_counter;
 	sprintf(new_co->thread_name,"%s",name);
 	assert(co_counter<CO_MAX);
@@ -55,28 +81,36 @@ void co_yield() {
 	do {
 		next_co = rand()%co_counter;
 		
-	} while(co_array[next_co]==NULL);
+	} while(co_array[next_co]==NULL || co_array[next_co]->wait!=NULL);
 		
-	
 	current = &(co_array[next_co]->uc);
+	
+	if(main_wait==NULL) {
+		if(rand()%(co_counter+1) == co_counter )
+			current=&main_uc;
+	}	
 	swapcontext(ccurrent  , current);
 	return ;	
 }
 
 void co_wait(struct co *thd) {
+	if(current == &main_uc) {
+		main_wait= &(thd->uc);
+	}
+	else {
+		for(int i=0;i<co_counter;i++) {
+			if( co_array[i]!=NULL     &&   (&(co_array[i]->uc) == current) )
+				co_array[i]->wait = &(thd->uc );
+		} 
+	}
 	ucontext_t * ccurrent = current;	
 	//printf("In co_wait: wait for %s\n",thd->thread_name);
 	current = &(thd->uc);
 	//getcontext(&thisuc);
 	printf("In co_wait: wait for %s\n",thd->thread_name);
-
-
 	int ret=swapcontext(ccurrent, current);
 	assert(ret!=-1);
 	printf("In co_wait: %s returned\n", thd->thread_name);	
-	current = ccurrent;	
-	co_array[thd->co_index]=NULL;
-	free(thd);
 	return;	
 }
 
