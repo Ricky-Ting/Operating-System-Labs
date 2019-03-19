12770a3ddc685135b24581fa2d7198b34a6eaf73
tracer-nju <tracer@nju.edu.cn>
2019-03-19 22:48:28 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 9294494d95444587b34920ee28415dec     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  22:48:28 up  2:47,  3 users,  load average: 0.24, 0.20, 0.14

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -1,18 +1,90 @@
 #include <common.h>
 #include <klib.h>
+#include <pthread.h>
 
 static uintptr_t pm_start, pm_end;
+
+#define ALI_F(x) ((x & 0x7)?( ((x>>3)<<3)+ 0x8 ) : x)
+#define SIZE(x)  (sizeof(x))
+
+struct __node_t{
+	node_t * next;
+	node_t * prev;
+	int size;
+	int used;
+} node_t;
+
+
+struct node_t * head; 
+pthread_mutex_t mylock;
+void List_Init(void) {
+	struct node_t * myhelp = malloc(sizeof())
+	
+}
+
 
 static void pmm_init() {
   pm_start = (uintptr_t)_heap.start;
   pm_end   = (uintptr_t)_heap.end;
+	head = ALI_F(pm_start);
+	head->next= head->prev = NULL;
+	head->used = 0;
+	head->size = pm_end - ALI_F(pm_start) - ALI_F(SIZE(struct node_t));
+	pthread_mutex_init(&mylock, NULL);
 }
 
+
+
+
 static void *kalloc(size_t size) {
-  return NULL;
+	pthread_mutex_lock(&mylock);
+	uintptr_t ret=NULL;
+	struct node_t * tmp = head;
+	while(tmp!=NULL && tmp->size < size) {
+		tmp=tmp->next;			
+	}
+	if(tmp!=NULL) {
+		ret = (void *)(tmp) + ALI_F( SIZE(struct node_t) );
+		tmp->used = 1;
+		if(  ((tmp->next==NULL)?pm_end:(void*)(tmp->next)) - ret - size > 2* ALI_F(SIZE(struct node_t))   ) {
+			struct node_t * tmp2 = ALI_F(ret+size);
+			tmp2->next = tmp->next;
+			if(tmp2->next != NULL)
+				tmp2->next->prev = tmp2;
+			tmp2->used = 0;
+			tmp->next = tmp2;
+			tmp2->size = ((tmp2->next==NULL)?pm_end:(void*)(tmp2->next))  - ALI_F( ( (void *)(tmp2) ));
+		}
+		
+	} 
+
+	pthread_mutex_unlock(&mylock);
+	return ret;
 }
 
 static void kfree(void *ptr) {
+	pthread_mutex_lock(&mylock);
+	struct node_t * midd = (struct node_t *)(ptr - ALI_F(SIZE(struct node_t)) );
+	midd->used = 0;
+	struct node_t * tmp = midd;
+	while( tmp->next!=NULL && tmp->next->used==0  ) {
+		tmp->next = tmp->next->next;
+		if(tmp->next->next!=NULL) 
+			tmp->next->next->prev = tmp;
+		tmp->size =  ((tmp->next->next==NULL)?pm_end:(void*)(tmp->next->next)) - ALI_F( ( (void *)(tmp) ) );
+		
+	}
+	tmp = midd;
+	while( tmp->prev!=NULL && tmp->prev->used==0  ) {
+		struct node_t * tmp2 = tmp->prev;
+		tmp2->next = tmp->next;
+		if(tmp->next!=NULL)
+			tmp->next->prev = tmp2;	
+		tmp2->size =  ((tmp->next==NULL)?pm_end:(void*)(tmp->next)) - ALI_F( ( (void *)(tmp2) ) );
+		tmp = tmp2;
+	}
+	pthread_mutex_unlock(&mylock);
+	return;
 }
 
 MODULE_DEF(pmm) {
