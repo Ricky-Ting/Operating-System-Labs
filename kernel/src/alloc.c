43430d5083b0e4e20735fb53b19375a9a89a9f06
tracer-nju <tracer@nju.edu.cn>
2019-03-19 23:39:44 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 9294494d95444587b34920ee28415dec     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  23:39:44 up  3:38,  3 users,  load average: 0.28, 0.24, 0.16

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -4,7 +4,7 @@
 
 static uintptr_t pm_start, pm_end;
 
-#define ALI_F(x) (   ( (uintptr_t(x)) & 0x7)?( (( (uintptr_t(x)) >>3)<<3)+ 0x8 ) : uintptr_t(x))
+#define ALI_F(x) (   ( (uintptr_t(x)) & 0x7)? ( ( ( (uintptr_t(x)) >>3)<<3)+ 0x8 ) : uintptr_t(x)  )
 //#define ALI_F(x) x
 #define SIZE(x)  (sizeof(x))
 
