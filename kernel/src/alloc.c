7ee9eab4ec73ab121b2342f1519de10a45b0a7a9
tracer-nju <tracer@nju.edu.cn>
2019-03-19 23:57:13 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 9294494d95444587b34920ee28415dec     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  23:57:13 up  3:56,  3 users,  load average: 0.25, 0.14, 0.11

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -28,7 +28,7 @@ uintptr_t func(uintptr_t x) {
 static void pmm_init() {
   pm_start = (uintptr_t)_heap.start;
   pm_end   = (uintptr_t)_heap.end;
-	myhead = (struct node_t *)(ALI_F(pm_start));
+	myhead = (ALI_F(pm_start));
 	myhead->next= myhead->prev = NULL;
 	myhead->used = 0;
 	myhead->size = pm_end - ALI_F(pm_start) - ALI_F(SIZE(struct node_t));
