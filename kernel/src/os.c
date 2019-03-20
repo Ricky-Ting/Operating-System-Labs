37334f22853a253b3fc50cf22c697ac9002d6bcf
tracer-nju <tracer@nju.edu.cn>
2019-03-20 20:46:19 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 5846ba8321094a4681a346fbfa75141b     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  20:46:19 up 52 min,  5 users,  load average: 0.13, 0.25, 0.22

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -18,11 +18,11 @@ static void os_run() {
   hello();
   _intr_write(1);
 	mytest();
-	/*	
+		
   while (1) {
     _yield();
   }
-	*/
+	
 }
 
 static _Context *os_trap(_Event ev, _Context *context) {
