cf69a43fb026165b01a8638604564d857ff6362d
tracer-nju <tracer@nju.edu.cn>
2019-03-20 20:58:57 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 5846ba8321094a4681a346fbfa75141b     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  20:58:57 up  1:04,  5 users,  load average: 0.09, 0.36, 0.34

--- kernel/src/test.c
+++ kernel/src/test.c
@@ -2,11 +2,11 @@
 #include <klib.h>
 
 void mytest(void){
-	int * a = pmm->alloc(sizeof(int));
+	int * a = pmm->alloc(100);
 	assert(a!=NULL);
 	printf("a=%d\n",a);
-	*a = 5;
-	assert( (*a == 5));
+	//*a = 5;
+	//assert( (*a == 5));
 
 	int * b = pmm->alloc(sizeof(int));
 	assert(b!=NULL);
