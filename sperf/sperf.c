4c87be9fba381112abeb0d74bfb5aec7128be28a
tracer-nju <tracer@nju.edu.cn>
2019-03-27 21:49:35 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 6f5b9fa74100438dbbf260eb07d7d21d     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  21:49:35 up  2:56,  3 users,  load average: 0.09, 0.04, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,3 +1,18 @@
+#include <sys/types.h>
+#include <unistd.h>
+
+
 int main(int argc, char *argv[]) {
+	pid_t pid = fork(); 
+	if(pid == 0) {
+		execve(argv[1], argv+2);
+	} else {
+
+				
+
+	}
+	
+
+
   return 0;
 }
