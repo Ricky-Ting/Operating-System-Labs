cb381ad818b4ba0602901b1e16b1971328e09ec0
tracer-nju <tracer@nju.edu.cn>
2019-03-27 21:52:37 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 6f5b9fa74100438dbbf260eb07d7d21d     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  21:52:37 up  2:59,  3 users,  load average: 0.31, 0.12, 0.04

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,11 +1,11 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
+extern char **environ;
 int main(int argc, char *argv[]) {
 	pid_t pid = fork(); 
 	if(pid == 0) {
-		execve(argv[1], argv+2, envp);
+		execve(argv[1], argv+2, environ);
 	} else {
 
 				
