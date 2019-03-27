ff31a08e5c910a09747ed79b3101a742a599b86e
tracer-nju <tracer@nju.edu.cn>
2019-03-27 22:18:53 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 6f5b9fa74100438dbbf260eb07d7d21d     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  22:18:53 up  3:25,  3 users,  load average: 0.27, 0.21, 0.13

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -8,15 +8,15 @@ int main(int argc, char *argv[]) {
 	printf("%s\n",argv[0]);
 	argv++;
 	printf("%s\n",argv[0]);
-	//pid_t pid = fork(); 
-	/*
+	pid_t pid = fork(); 
+	
 	if(pid == 0) {
-		int ret = execve("/bin/ls", argv++, environ);
+		int ret = execve("/bin/ls", argv, environ);
 		printf("Shouldn't be here! error=%d\n",ret);
 	} else {
 		printf("End\n");
 	}
-	*/
+	
 
 
   return 0;
