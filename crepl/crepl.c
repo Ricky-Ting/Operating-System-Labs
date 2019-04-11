51c9562e57ec4de7f88763a34979b9809caa2cd5
tracer-nju <tracer@nju.edu.cn>
2019-04-11 19:01:30 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 8a21e16ab70c4ab88ba722f4f3eea694     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  19:01:31 up 18 min,  4 users,  load average: 1.56, 1.77, 1.37

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+#define MAXBUF 4028
+
 int main(int argc, char *argv[]) {
+	
+	char ** line;
+	printf(">> ");
+	while( getline(line, MAXBUF) ) {
+		if(judge(line) == QUIT) {
+			printf("\n Hello World!\n");
+			return 0;
+		} else if(judge(line) == FUNC) {
+			
+
+		}
+		else {
+
+
+		}
+
+	}
+
   return 0;
 }
