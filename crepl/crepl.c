cdb8b55a78cca60a8c0e8bfa7b3f7b19e14dee25
tracer-nju <tracer@nju.edu.cn>
2019-04-11 21:21:24 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 8a21e16ab70c4ab88ba722f4f3eea694     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  21:21:24 up  2:38,  4 users,  load average: 0.34, 0.14, 0.04

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
 			sprintf(GCC, "gcc -shared -fPIC -m%d %s -o sl.so", (int)(8*(sizeof(void *)) ), file_tmplate);			
 			system(GCC);
 		
-			void * handle = dlopen("sl.so", RTLD_NOW | RTLD_NODELETE);
+			dlopen("sl.so", RTLD_NOW | RTLD_NODELETE);
 	
 			unlink(file_tmplate);
 
