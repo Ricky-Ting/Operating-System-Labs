7cd03a96577e73b711ab0d75fe128d7427bc397f
tracer-nju <tracer@nju.edu.cn>
2019-04-11 19:13:47 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 8a21e16ab70c4ab88ba722f4f3eea694     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  19:13:47 up 30 min,  4 users,  load average: 0.25, 0.34, 0.73

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -3,12 +3,11 @@
 #define QUIT 0
 #define FUNC 1
 #define EXPR 2 
-#define buf[x] *(buf+i)
 
 int judge(char ** buf) {
-	if( buf[0] == 'q' && buf[1] == 'u' && buf[2] == 'i' && buf[3]=='t' )
+	if( *(buf+0) == 'q' && *(buf+1) == 'u' &&  *(buf+2) == 'i' &&  *(buf+3) =='t' )
 		return QUIT;
-	else if(buf[0] == 'i' && buf[1] == 'n' && buf[2] == 't')
+	else if( *(buf+0) == 'i' && *(buf+1) == 'n' &&  *(buf+2) == 't' )
 		return FUNC;
 	else
 		return EXPR;
