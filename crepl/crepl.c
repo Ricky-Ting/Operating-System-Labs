1dc30d1d14400940cfed5557284df4854fcb7104
tracer-nju <tracer@nju.edu.cn>
2019-04-11 19:57:45 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 8a21e16ab70c4ab88ba722f4f3eea694     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  19:57:45 up  1:14,  4 users,  load average: 0.32, 0.19, 0.12

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 		} else if(judge(line) == FUNC) {
 			int tmpfd = mkstemp("Hello");
 			Assert( (tmpfd!=-1),"\nCannot Create tmp File\n");	
-			char tmpfilename[MAX_F_LEN];
+			char tmpfilename[MAX_F_LEN], buf[MAX_F_LEN];
 			snprintf(tmpfilename, MAX_F_LEN, "/proc/%ld/fd/%d", (long)getpid(), tmpfd);	
 			int ret = (readlink(tmpfilename, buf, MAX_F_LEN);
 			Assert( (ret>=0), "\n readlink() failed \n");	
