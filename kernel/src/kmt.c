e64e5798169504d0c99f6689c5b5d72e7b63713c
tracer-nju <tracer@nju.edu.cn>
2019-05-08 02:24:09 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: d81bcbb39cf04517974e58f853be6e44     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  02:24:09 up  7:21,  7 users,  load average: 0.86, 1.22, 0.98

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -205,7 +205,7 @@ void kmt_sem_wait(sem_t *sem) {
 		TRACE_ENTRY;
 	}
 	TRACE_EXIT;
-	//printf("%s get the lock %s\n", current->name, sem->name);
+	printf("%s get the lock %s\n", current->name, sem->name);
 	kmt_spin_unlock(&sem->lock);
 }
 
@@ -221,7 +221,7 @@ void kmt_sem_signal(sem_t *sem) {
 		//printf("Here %d %d %s\n",sem->tail, sem->head, sem->name);
 
 	}	
-	//printf("%s release the lock %s\n", current->name, sem->name);
+	printf("%s release the lock %s\n", current->name, sem->name);
 
 	kmt_spin_unlock(&sem->lock);
 	TRACE_ENTRY;
