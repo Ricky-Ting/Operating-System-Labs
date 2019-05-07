a98128f5b8479fb5a6a68dd898c43e45f5afd225
tracer-nju <tracer@nju.edu.cn>
2019-05-08 01:21:45 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: d81bcbb39cf04517974e58f853be6e44     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  01:21:45 up  6:19,  7 users,  load average: 0.72, 0.81, 0.75

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -40,10 +40,10 @@ _Context* kmt_context_save(_Event event, _Context * context) {
 _Context* kmt_context_switch(_Event event, _Context * context) {
 	task_t *iter = task_head[_cpu()];
 	//printf("This is cpu %d\n",_cpu());
- 	//printf("List \n");
+ 	printf("List \n");
 
 	while(iter!=NULL && iter->status!=TASK_READY) {
-		//printf("%s\n",iter->name);
+		printf("%s\n",iter->name);
 		iter = iter->next;
 	}
 	if(iter==NULL) {
--- kernel/src/os.c
+++ kernel/src/os.c
@@ -35,10 +35,10 @@ static void os_init() {
 	kmt->init();
 	dev->init();
 	//vfs->init();
-	kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
-	kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
-	//kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
-	//kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");
+	kmt->create(pmm->alloc(sizeof(task_t)), "print1", echo_task, "tty1");
+	kmt->create(pmm->alloc(sizeof(task_t)), "print2", echo_task, "tty2");
+	kmt->create(pmm->alloc(sizeof(task_t)), "print3", echo_task, "tty3");
+	kmt->create(pmm->alloc(sizeof(task_t)), "print4", echo_task, "tty4");
 
 };
 static void hello() {
