4465daafd18ebae8ab47b23af23b533d5336895a
tracer-nju <tracer@nju.edu.cn>
2019-05-08 00:28:33 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: d81bcbb39cf04517974e58f853be6e44     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  00:28:33 up  5:26,  7 users,  load average: 0.16, 0.51, 0.61

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -181,6 +181,7 @@ void kmt_sem_wait(sem_t *sem) {
 			}
 			sem->tail = (sem->tail + 1) % MAXQ;
 			current->status = TASK_SLEEP;	
+			printf("%s to sleep\n", current->name);
 		}
 		TRACE_EXIT;
 		kmt_spin_unlock(&sem->lock);
@@ -199,6 +200,7 @@ void kmt_sem_signal(sem_t *sem) {
 	if(sem->queue[sem->head]!=NULL) {
 		//printf("Here %d %d\n",sem->tail, sem->head);
 		sem->queue[sem->head]->status = TASK_READY;
+		printf("Wake %s\n", sem->queue[sem->head]->name);
 		sem->queue[sem->head] = NULL;
 		sem->head = (sem->head + 1) % MAXQ;
 	}	
