23059ade9be43808b00304a2e8825ef1e37fa6ac
tracer-nju <tracer@nju.edu.cn>
2019-05-07 15:28:50 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: a7f15dc0503240a6bd2ecf7f85496ffb     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  15:28:50 up 33 min,  4 users,  load average: 0.85, 1.08, 1.04

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -85,11 +85,11 @@ void kmt_sem_init(sem_t *sem, const char *name, int value){
 
 }
 
-void sem_wait(sem_t *sem) {
+void kmt_sem_wait(sem_t *sem) {
 
 }
 
-void sem_signal(sem_t *sem) {
+void kmt_sem_signal(sem_t *sem) {
 
 }
 
