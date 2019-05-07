2558047f63b2dc6e9312ce2795aa52be937bb500
tracer-nju <tracer@nju.edu.cn>
2019-05-08 00:09:34 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: d81bcbb39cf04517974e58f853be6e44     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  00:09:34 up  5:07,  7 users,  load average: 0.72, 0.66, 0.57

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -35,10 +35,10 @@ static void os_init() {
 	kmt->init();
 	dev->init();
 	//vfs->init();
-	//kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
-	//kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
-	//kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
-	//kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");
+	kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
+	kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
+	kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
+	kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");
 
 };
 static void hello() {
