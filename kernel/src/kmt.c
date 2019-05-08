8eeeebfda1dc772f05a9437182584f86ab56355a
tracer-nju <tracer@nju.edu.cn>
2019-05-08 10:26:30 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 628a539ede624a98a3d1c28708c33993     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  10:26:30 up 12 min,  4 users,  load average: 1.26, 1.81, 1.07

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -42,6 +42,7 @@ _Context* kmt_context_save(_Event event, _Context * context) {
 
 _Context* kmt_context_switch(_Event event, _Context * context) {
 	task_t *iter = task_head[_cpu()];
+	assert(0);
 	//printf("This is cpu %d\n",_cpu());
  	//printf("List \n");
 
