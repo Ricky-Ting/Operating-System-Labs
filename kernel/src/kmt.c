0d49129a49f8ea852b492ab30e5da6f667fb2a09
tracer-nju <tracer@nju.edu.cn>
2019-05-08 12:38:02 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: c0a7e99c77974d93a21da414597c3cb6     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  12:38:02 up 6 min,  4 users,  load average: 0.60, 0.86, 0.45

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -36,6 +36,7 @@ void kmt_init() {
 _Context* kmt_context_save(_Event event, _Context * context) {
 
 	if(current)	{
+		printf("%s\n", current->name);
 		assert(current->context == context);
 		current->context = context;
 	}
