a96aa1077a54b661acadbdfb3156be50fc9e5de8
tracer-nju <tracer@nju.edu.cn>
2019-05-08 11:18:47 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 628a539ede624a98a3d1c28708c33993     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  11:18:47 up  1:05,  4 users,  load average: 0.23, 0.52, 0.50

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -42,6 +42,7 @@ _Context* kmt_context_save(_Event event, _Context * context) {
 
 _Context* kmt_context_switch(_Event event, _Context * context) {
 	task_t *iter = task_head[_cpu()];
+	_Context* ret;
 	//assert(0);
 	/*
 	for(int i=0; i<MAXCPU; i++) {
@@ -56,14 +57,14 @@ _Context* kmt_context_switch(_Event event, _Context * context) {
 	//assert(0);
 	//printf("This is cpu %d\n",_cpu());
  	//printf("List \n");
-
+	printf("%d CPU in switch!\n",_cpu());
 	while(iter!=NULL && iter->status!=TASK_READY) {
 		//printf("%s\n",iter->name);
 		iter = iter->next;
 	}
 	if(iter==NULL) {
 		//printf("Schedule %s\n",current->name);
-		return context;	
+		ret = context;	
 	}	else {
 		if(task_tail[_cpu()]!=iter) {
 			if(task_head[_cpu()] == iter && iter->next!=NULL) {
@@ -83,8 +84,11 @@ _Context* kmt_context_switch(_Event event, _Context * context) {
 		}	
 		current = iter;
 		printf("Schedule %s\n",current->name);
-		return (iter->context);	
+		ret = (iter->context);	
 	}
+	printf("%d CPU out switch\n",_cpu());
+
+	return ret;
 }
 
 
