8a9b4aee71e68bdce52a213b666acdb0e2b13700
tracer-nju <tracer@nju.edu.cn>
2019-05-18 20:01:55 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 2a8e6000a9ae4f2c9728f421dc68f34a     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  20:01:55 up  6:36,  4 users,  load average: 0.24, 0.15, 0.11

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -170,7 +170,7 @@ static inline int search_in_entry(void * entry_start) {
 		char shasum[MAXBUF];
 		if(pid==0) {
 			//char* myenv[4] = {"sha1sum", "-c","-", NULL}; 
-			//dup2(pipe1[1],1);
+			dup2(pipe1[1],1);
 			dup2(pipe2[0],0);
 			char buff;
 			scanf("%c",&buff);
