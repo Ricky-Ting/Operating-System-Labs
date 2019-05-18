916d79ea17ebf2adb6d4a3523ddf3d094b5aee13
tracer-nju <tracer@nju.edu.cn>
2019-05-18 15:18:11 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 2a8e6000a9ae4f2c9728f421dc68f34a     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  15:18:11 up  1:52,  4 users,  load average: 0.19, 0.25, 0.18

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <sys/mman.h>
+
+int fd;
 
 int main(int argc, char *argv[]) {
-	printf("%s",argv[1]);
+
+	/*Open the img*/
+	fd = open(argv[1], O_RDONLY);
+	assert(fd>=0);
+
+	/*Get the size of the img*/
+	struct stat st;
+	int ret = fstat(fd,&st);
+	assert(ret!=-1);
+	int len = st.st_size;
+	
+	/*Map the file to mem*/
+
+	mmap(NULL, len, PROT_READ, MAP_SHARED, fd, 0);
+
+
+
+
+	close(fd);
   return 0;
 }
