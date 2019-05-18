8608992818ac06f82380e2a6ae5a4cd6a4d13c9f
tracer-nju <tracer@nju.edu.cn>
2019-05-18 15:42:34 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 2a8e6000a9ae4f2c9728f421dc68f34a     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  15:42:34 up  2:16,  4 users,  load average: 0.10, 0.09, 0.09

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -5,6 +5,19 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define BytesPerSectorOff 0x0B
+#define SectorsPerClusterOff 0x0D
+#define ReservedSectorOff 0x0e
+#define NumberofFATOff 0x10
+#define SectorsPerFATOff 0x24
+
+uint16_t BytesPerSector;
+uint8_t SectorsPerCluster;
+uint16_t ReservedSector;
+uint8_t NumberofFAT;
+uint32_t SectorsPerFAT;
+
+
 int fd;
 void * img_start;
 
@@ -23,10 +36,23 @@ int main(int argc, char *argv[]) {
 	/*Map the file to mem*/
 
 	img_start = mmap(NULL, len, PROT_READ, MAP_SHARED, fd, 0);
+	assert(img_start != NULL);
+
+	/*
 	for(int i=0;i<10;i++) {
 		printf("%x ", (unsigned int) ( *(unsigned char *)(img_start + i)) );
 	}
+	*/
 
+
+	/* Collect basic info*/
+	BytesPerSector = *((uint16_t *)(img_start + BytesPerSectorOff));
+	SectorsPerCluster = *((uint8_t *)(img_start + SectorsPerClusterOff));
+	ReservedSector = *((uint16_t *) (img_start + ReservedSectorOff));
+	NumberofFAT = *((uint8_t *)(img_start + NumberofFATOff));
+	SectorsPerFAT = *((uint32_t *)(img_start + SectorsPerFATOff));		
+		
+	printf("%x %x %x %x %x", BytesPerSector, SectorPerCluster, reservedSector, NumberofFAT, SectorsPerFAT);
 
 
 
