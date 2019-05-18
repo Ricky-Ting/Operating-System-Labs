69b95d1c3faaa2639f36b499f0818ac3a60cb1a6
tracer-nju <tracer@nju.edu.cn>
2019-05-18 20:01:01 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 2a8e6000a9ae4f2c9728f421dc68f34a     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  20:01:01 up  6:35,  4 users,  load average: 0.32, 0.14, 0.11

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -172,9 +172,9 @@ static inline int search_in_entry(void * entry_start) {
 			//char* myenv[4] = {"sha1sum", "-c","-", NULL}; 
 			//dup2(pipe1[1],1);
 			dup2(pipe2[0],0);
-			char buf;
-			scanf("%c",buf);
-			printf("%c\n", buf);
+			char buff;
+			scanf("%c",buff);
+			printf("%c\n", buff);
 			//execvp("sha1sum", myenv);
 			printf("Shouldn't be here !\n");
 			exit(0);
