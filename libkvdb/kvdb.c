7e48613c362492832c6680b4dce8468705ec1e7e
tracer-nju <tracer@nju.edu.cn>
2019-05-21 22:18:58 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 8af35450871246c3a46ade2457d05613     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  22:18:58 up  3:06,  5 users,  load average: 0.00, 0.04, 0.07

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -6,7 +6,7 @@
 
 int kvdb_open(kvdb_t *db, const char *filename) {
 	/* What if the opened db isn't closed */
-	memcpy(db->filename, filename, strlen(filename) + 1);
+	//memcpy(db->filename, filename, strlen(filename) + 1);
 
 	db->fp = fopen(filename, "w+");	 
 	if(db->fp==NULL) {
