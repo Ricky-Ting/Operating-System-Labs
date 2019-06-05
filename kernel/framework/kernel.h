9eb2e5ae0dca28aa5b2e053763b70c9313dc278c
tracer-nju <tracer@nju.edu.cn>
2019-06-05 17:24:14 +0800

    Static hostname: rickyting-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: c1ded8b4eaaf4397923b14f0e7f37a75            Boot ID: 1ca4b6a34f4344cd8f0d3c8e1a4ab2e8     Virtualization: oracle   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  17:24:15 up 40 min,  4 users,  load average: 0.29, 0.30, 0.36

--- kernel/framework/kernel.h
+++ kernel/framework/kernel.h
@@ -42,6 +42,14 @@ typedef struct {
 } MODULE(kmt);
 
 typedef struct device device_t;
+struct device {
+  const char *name;
+  int id;
+  void *ptr;
+  devops_t *ops;
+};
+
+
 typedef struct devops {
   int (*init)(device_t *dev);
   ssize_t (*read)(device_t *dev, off_t offset, void *buf, size_t count);
